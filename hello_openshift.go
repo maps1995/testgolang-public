dc00f61eb5937520c89bbea0fa68e8bf40a1d633
maps1995 <127659290+maps1995@users.noreply.github.com>
2023-12-18 12:27:48 +0330

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift! new build1"
+		response = "Hello OpenShift! new build2"
 	}
 
 	fmt.Fprintln(w, response)
