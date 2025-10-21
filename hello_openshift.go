abbcc33e97945cd379fc1fbefb4e150ca2956d5d
mahdi.abbasi <mahdi.abbasi@parspack.com>
2025-10-21 14:44:43 +0330

    Commit 2

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift! new build 2"
+		response = "Hello OpenShift! new build 22"
 	}
 
 	fmt.Fprintln(w, response)
